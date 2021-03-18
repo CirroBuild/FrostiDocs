37d7f062dad6cfa264ac420b3e5afe4493bc3cfd
Brian Thomas Smith <brian@smith.berlin>
2021-03-18 19:12:56 +0100

    chore: add Cloud Engineer role (#406)

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -107,8 +107,8 @@ const CareersPage = () => {
             </p>
           </div>
           <div className={clsx(caCss.card__side, caCss["card__side--center"])}>
-            <a className={joCss.job} href="senior-cloud-architect/">
-              <h3 className={joCss.job__title}>Senior Cloud Architect</h3>
+            <a className={joCss.job} href="senior-cloud-engineer/">
+              <h3 className={joCss.job__title}>Senior Cloud Engineer</h3>
               <p className={joCss.job__location}>Remote</p>
               <span className={joCss.job__cta}>
                 Details&nbsp;
