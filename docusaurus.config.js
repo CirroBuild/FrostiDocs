f558cf1b40a04fb0a863d03d588b749ae2ff77bd
Brian Thomas Smith <brian@smith.berlin>
2021-06-01 18:20:50 +0200

    docs: bump software versions for 6.0.3 (#474)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,13 +15,13 @@ const customFields = {
   domain,
   githubOrgUrl,
   githubUrl: `${githubOrgUrl}/questdb`,
-  helmVersion: "0.4.0",
+  helmVersion: "0.8.0",
   linkedInUrl: "https://www.linkedin.com/company/questdb/",
   oneLiner: "Fast SQL open source database for time series - QuestDB",
   slackUrl: `https://slack.${domain}`,
   stackoverflowUrl: "https://stackoverflow.com/questions/tagged/questdb",
   twitterUrl: "https://twitter.com/questdb",
-  version: "6.0.2",
+  version: "6.0.3",
   videosUrl: "https://www.youtube.com/channel/UChqKEmOyiD9c6QFx2mjKwiA",
 }
 
