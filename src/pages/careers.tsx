18280243bd57776e2a42961aab2a1f23a4b03496
Brian Thomas Smith <brian@smith.berlin>
2022-01-10 12:23:03 +0100

    chore: job spec edits (#671)
    * chore: remove C++
    
    * chore: job spec edits
    
    * Typo: 'As a Senior'

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -113,7 +113,22 @@ const CareersPage = () => {
               </span>
             </a>
             <a className={joCss.job} href="developer-in-test/">
-              <h3 className={joCss.job__title}>Developer in Test</h3>
+              <h3 className={joCss.job__title}>Developers in Test</h3>
+              <p className={joCss.job__location}>Remote</p>
+              <span className={joCss.job__cta}>
+                Details&nbsp;
+                <img
+                  alt="Right arrow"
+                  height={20}
+                  src="/img/pages/careers/arrowRight.svg"
+                  width={20}
+                />
+              </span>
+            </a>
+            <a className={joCss.job} href="developer-relations-engineer/">
+              <h3 className={joCss.job__title}>
+                Developer Relations Engineers
+              </h3>
               <p className={joCss.job__location}>Remote</p>
               <span className={joCss.job__cta}>
                 Details&nbsp;
@@ -140,21 +155,6 @@ const CareersPage = () => {
             </a>
             <a className={joCss.job} href="senior-cloud-engineer/">
               <h3 className={joCss.job__title}>Cloud Engineers</h3>
-              <p className={joCss.job__location}>Remote</p>
-              <span className={joCss.job__cta}>
-                Details&nbsp;
-                <img
-                  alt="Right arrow"
-                  height={20}
-                  src="/img/pages/careers/arrowRight.svg"
-                  width={20}
-                />
-              </span>
-            </a>
-            <a className={joCss.job} href="developer-relations-engineer/">
-              <h3 className={joCss.job__title}>
-                Developer Relations Engineers
-              </h3>
               <p className={joCss.job__location}>Remote</p>
               <span className={joCss.job__cta}>
                 Details&nbsp;
