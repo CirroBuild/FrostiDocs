c0220579ed53389754ceb4d94dd1620e63fd9289
Arijus Šukys <argshook@gmail.com>
2022-01-19 11:59:01 +0200

    fix `plugins/ts-compile.sh` after yarn upgrade (#682)
    updating dependencies brings in a different dependency tree which
    `plugins/ts-compile.sh` is not happy about.
    
    this commit implements `tsconfig.json` for `plugins` with `skipLibCheck`
    to avoid the typescript type collision error

--- plugins/ts-compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Compile "tutorial" plugin
-tsc \
-    --esModuleInterop \
-    --outDir \
-    plugins/tutorial/compiled \
-    plugins/tutorial/index.ts plugins/tutorial/markdownLoader.ts
