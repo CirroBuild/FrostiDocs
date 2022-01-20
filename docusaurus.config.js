e6e278a4b86ebbb9b0c73de5945e53acd76180ef
Arijus Šukys <argshook@gmail.com>
2022-01-20 16:01:02 +0200

    add `java` syntax highlighting config (#688)
    Co-authored-by: Brian Thomas Smith <brian@smith.berlin>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -101,7 +101,7 @@ const config = {
     },
     prism: {
       defaultLanguage: "questdb-sql",
-      additionalLanguages: ["rust", "csharp", "julia", "cpp"],
+      additionalLanguages: ["rust", "csharp", "julia", "cpp", "java"],
       theme: require("./src/internals/prism-github"),
       darkTheme: require("./src/internals/prism-dracula"),
     },
