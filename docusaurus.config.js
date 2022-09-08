dff6b8567b397ce323f7d04f86084ba94b26dc0c
Arijus Šukys <argshook@gmail.com>
2022-09-08 16:29:11 +0300

    sitemap trailing slash (#1032)
    * add trailing slash to sitemap
    
    * align netlify file
    
    * add trailing slash to redirects
    
    * remove double redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -331,11 +331,9 @@ const config = {
         },
 
         sitemap: {
-          // Removed: https://github.com/ekalinin/sitemap.js/blob/master/CHANGELOG.md#50-breaking-changes
-          // cacheTime: 600 * 1000, // 600 sec - cache purge period
           changefreq: "daily",
           priority: 0.7,
-          trailingSlash: false,
+          trailingSlash: true,
         },
         theme: {
           customCss: [
