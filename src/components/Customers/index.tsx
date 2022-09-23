c0462d73d8fda04b14d8c2a093cec7d827822f1b
Arijus Šukys <argshook@gmail.com>
2022-09-23 17:44:37 +0300

    marquee customers (#1057)
    * add .nvmrc
    
    to be consistent between our libraries
    
    * add marquee to scroll customer logos
    
    * adjust styles
    
    * fix light theme

--- src/components/Customers/index.tsx
+++ src/components/Customers/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react"
-import clsx from "clsx"
-
-import cuCss from "../../css/index/customer.module.css"
-import seCss from "../../css/section.module.css"
+import React from "react"
+import { Section } from "../Section"
+import styles from "./styles.module.css"
 import { logos } from "../../assets/logos"
 import { CustomerLogo } from "../../assets/types"
+import Marquee from "react-fast-marquee"
+import useThemeContext from "@theme/hooks/useThemeContext"
+import clsx from "clsx"
 
 const logosOverrides: Record<keyof typeof logos, Partial<CustomerLogo>> = {
   yahoo: {
@@ -38,69 +39,28 @@ const customerLogos = [
 ].map((key) => ({ ...logos[key], ...logosOverrides[key] }))
 
 const Customers = () => {
-  const nbElements = 6
-  const nbPanels = Math.ceil(customerLogos.length / nbElements)
-  const panels = Array.from({ length: nbPanels }, (_, index) => index + 1)
-
-  const [index, setIndex] = useState<number>(1)
-
-  const handleClickControl = (index: number) => {
-    setIndex(index)
-  }
-
+  const { isDarkTheme } = useThemeContext()
   return (
-    <section className={clsx(seCss["section--slim"])}>
-      <div className={cuCss.logo__desktop}>
-        {customerLogos.map((logo) => (
-          <div key={logo.alt} className={cuCss.logo__item}>
-            <img
-              alt={logo.alt}
-              height={logo.height}
-              src={logo.src}
-              width={logo.width}
-              style={{ top: logo.offset ?? 0 }}
-            />
-          </div>
-        ))}
-      </div>
-      <div className={cuCss.logo__mobile}>
-        {panels.map((i: number) => {
-          const panelLogos = customerLogos.slice(
-            (i - 1) * nbElements,
-            i * nbElements,
-          )
-          const active = i === index ? cuCss.logo__mobile_panel_active : ""
-          return (
-            <div key={i} className={`${cuCss.logo__mobile_panel} ${active}`}>
-              {panelLogos.map((panelLogo) => (
-                <div key={panelLogo.alt} className={cuCss.logo__item}>
-                  <img
-                    alt={panelLogo.alt}
-                    height={panelLogo.height}
-                    src={panelLogo.src}
-                    width={panelLogo.width}
-                    style={{ top: panelLogo.offset ?? 0 }}
-                  />
-                </div>
-              ))}
-            </div>
-          )
-        })}
-        <div className={cuCss.logo__mobile_nav}>
-          {panels.map((i: number) => {
-            const active =
-              i === index ? cuCss.logo__mobile_nav__control_active : ""
-            return (
-              <div
-                key={i}
-                className={`${cuCss.logo__mobile_nav__control} ${active}`}
-                onClick={() => handleClickControl(i)}
+    <Section noGap>
+      <Marquee gradientColor={isDarkTheme ? [33, 34, 44] : [255, 255, 255]}>
+        <div className={styles.logos}>
+          {customerLogos.map((logo) => (
+            <div
+              key={logo.alt}
+              className={clsx(styles.logo, { [styles.logoDark]: isDarkTheme })}
+            >
+              <img
+                alt={logo.alt}
+                height={logo.height}
+                src={logo.src}
+                width={logo.width}
+                style={{ top: logo.offset ?? 0 }}
               />
-            )
-          })}
+            </div>
+          ))}
         </div>
-      </div>
-    </section>
+      </Marquee>
+    </Section>
   )
 }
 
