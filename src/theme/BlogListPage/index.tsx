59ccd6e8b4b62af83109040d570e3f7959154cec
Kai Strubel <strubel.kai@gmail.com>
2023-01-28 11:26:00 -0800

    continuing to set up blog page

--- src/theme/BlogListPage/index.tsx
+++ src/theme/BlogListPage/index.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Layout from "@theme/Layout"
-import BlogPostItem from "@theme/BlogPostItem"
 import BlogListPaginator from "@theme/BlogListPaginator"
 import type { FrontMatter as OriginalFrontMatter } from "@theme/BlogPostPage"
 import type { Props } from "@theme/BlogListPage"
@@ -10,59 +9,14 @@ import { ThemeClassNames } from "@docusaurus/theme-common"
 
 import styles from "./styles.module.css"
 import { ListItem } from "./ListItem"
-import { Categories } from "./Categories"
-import type { Props as CategoriesProps } from "./Categories"
-import { Chips } from "./Chips"
-import type { Props as ChipProps } from "./Chips"
 import Subscribe from "../../components/Subscribe"
 import ActionCard from "../../components/ActionCard"
 import SubscribeIcon from "../../components/ActionFooter/subscribeIcon.svg"
 
 export type FrontMatter = OriginalFrontMatter & { permalink?: string }
 
-const categories: CategoriesProps["categories"] = [
-  {
-    title: "Benchmarks",
-    description:
-      "Reproducible benchmarks of QuestDB and other databases using open source benchmarking frameworks",
-    url: "/blog/tags/benchmark",
-  },
-  {
-    title: "Demos",
-    description:
-      "Demos involving QuestDB and other popular open source tools for a wide range of use cases",
-    url: "/blog/tags/demo",
-  },
-  {
-    title: "Tutorials",
-    description:
-      "Step-by-step tutorials and guides for developers to build applications with QuestDB",
-    url: "/blog/tags/tutorial",
-  },
-  {
-    title: "User Stories",
-    description:
-      "How QuestDB powers the core infrastructure of our users for time series data and real-time analytics",
-    url: "/customers",
-  },
-]
 
-const prioritizedTags: ChipProps["items"] = [
-  "release",
-  "engineering",
-  "python",
-  "grafana",
-  "crypto",
-  "sql",
-  "kafka",
-  "prometheus",
-  "telegraf",
-  "company",
-  "community",
-].map((tag) => ({
-  name: tag,
-  permalink: `/blog/tags/${tag.replace(/ /g, "-")}`,
-}))
+
 
 function BlogListPage(props: Props): JSX.Element {
   const { metadata, items } = props
@@ -76,8 +30,7 @@ function BlogListPage(props: Props): JSX.Element {
   const title = isBlogOnlyMode ? siteTitle : blogTitle
 
   const posts = [...items]
-  const latestPost = metadata.page === 1 ? posts.shift() : undefined
-
+  
   return (
     <Layout
       title={title}
@@ -90,37 +43,7 @@ function BlogListPage(props: Props): JSX.Element {
       }}
     >
       <main className={styles.root}>
-        {latestPost !== undefined && (
-          <div className={styles.latestPost}>
-            <BlogPostItem
-              key={latestPost.content.metadata.permalink}
-              frontMatter={latestPost.content.frontMatter}
-              metadata={{
-                ...latestPost.content.metadata,
-                permalink:
-                  (latestPost.content.frontMatter as FrontMatter).permalink ??
-                  latestPost.content.metadata.permalink,
-                tags: [],
-              }}
-              truncated={latestPost.content.metadata.truncated}
-            >
-              {React.createElement(latestPost.content)}
-            </BlogPostItem>
-          </div>
-        )}
-
-        <h2>Popular topics</h2>
-        {/* BlogListPage component is used for blog and for tags.
-                When rendered for tags, then `metadata` includes tag, instead of blog data */}
-        <Categories
-          activeCategory={((metadata as unknown) as Tag).permalink}
-          categories={categories}
-        />
-        <Chips
-          activeChip={((metadata as unknown) as Tag).permalink}
-          items={prioritizedTags}
-        />
-
+      
         <h2>
           {isTagsPage
             ? `Articles tagged with "${((metadata as unknown) as Tag).name}"`
